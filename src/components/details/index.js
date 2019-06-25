7196fa6987f60861cf6db0998483f505886dc8f2
YTTTTY <13112688275@163.com>
2019-06-25 17:43:46 +0800

    css

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -5,8 +5,11 @@ import style from './details.scss';
 class details extends React.Component {
     render() {
         return (
-            <div className={style.input}>
-                <div className={style.title}>details</div>
+            <div className={style.page}>
+                <div className={style.card}>
+                    <h1 className={style.title}>X战警：黑凤凰</h1>
+                    <div className={style.subjectInfo}></div>
+                </div>
             </div>
         )
     }
