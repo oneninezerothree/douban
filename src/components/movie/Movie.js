3ccc27cf8d637aca2f0dc17c4b504d8778da6205
zfg <2548035563@qq.com>
2019-06-27 20:16:30 +0800

    小改动

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -15,12 +15,10 @@ export default class Movie extends Component {
     }
 
     requestData = () => {
-        // "https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=广州&start=0&count=10"
         fetchJSONP("https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=广州&start=0&count=10")
             .then(response=>response.json())
             .then(data=>{
                 this.state.movieList.push(data);
-                // this.setState({movieList:[data]})
                 this.setState({movieList:this.state.movieList})
             })
         fetchJSONP("https://api.douban.com/v2/movie/coming_soon?apikey=0b2bdeda43b5688921839c8ecb20399b&start=0&count=10")
@@ -35,7 +33,6 @@ export default class Movie extends Component {
         .then(data=>{
             this.state.movieList.push(data);
             this.setState({movieList:this.state.movieList})
-            console.log(this.state.movieList)
         })
         
     }
@@ -74,7 +71,7 @@ export default class Movie extends Component {
                                                 item.subjects.map((item,index)=>{
                                                     return(
                                                         <li className={movie.item} key={index}>
-                                                            <a href="/movie/subject/1291561?refer=home">
+                                                            <a href="###">
                                                                 <div className={movie.itemposter} style={{"backgroundImage": `url(${item.images.large})`}}></div>
                                                                 <span className={movie.itemtitle}>{item.title}</span>
                                                                 <div className={movie.itemrating}>
@@ -108,34 +105,34 @@ export default class Movie extends Component {
                         <div className={movie.sectioncontent}>                             
                             <ul>  
                                 <li style={{borderColor:'#4F9DED'}}>
-                                    <a href="https://m.douban.com/doulist/968362/" style={{color:'#4F9DED'}}>同时入选IMDB250和豆瓣电影250的电影</a>
+                                    <a href="###" style={{color:'#4F9DED'}}>同时入选IMDB250和豆瓣电影250的电影</a>
                                 </li>
                                 <li style={{borderColor: '#FF4055'}}>
-                                    <a href="https://m.douban.com/doulist/16002/" style={{color: '#FF4055'}}>带你进入不正常的世界</a>
+                                    <a href="###" style={{color: '#FF4055'}}>带你进入不正常的世界</a>
                                 </li>
                                 <li style={{borderColor: '#2384E8'}}>
-                                    <a href="https://m.douban.com/doulist/190343/" style={{color: '#2384E8'}}>用电【影】来祭奠逝去的岁月</a>
+                                    <a href="###" style={{color: '#2384E8'}}>用电【影】来祭奠逝去的岁月</a>
                                 </li>
                                 <li style={{borderColor: '#FFC46C'}}>
-                                    <a href="https://m.douban.com/doulist/1125971/" style={{color: '#FFC46C'}}>女孩们的故事【电影】</a>
+                                    <a href="###" style={{color: '#FFC46C'}}>女孩们的故事【电影】</a>
                                 </li>
                                 <li className={movie.line}></li>
                                 <li style={{borderColor: '#42BD56'}}>
-                                    <a href="https://www.douban.com/doubanapp/dispatch?copy_open=1&amp;from=mdouban&amp;download=1&amp;model=B&amp;copy=1&amp;page=movie&amp;channel=card_find_movie&amp;uri=%2Fmovie%2Ftag" style={{color: '#42BD56'}}>
+                                    <a href="###" style={{color: '#42BD56'}}>
                                         使用 App 【找电影】功能
                                     </a>
                                 </li>
                                 <li style={{borderColor: '#42BD56'}}>
-                                    <a href="https://m.douban.com/doulist/4253902/" style={{color: '#42BD56'}}>科幻是未来的钥匙。【科幻题材佳作·收藏必备】</a>
+                                    <a href="###" style={{color: '#42BD56'}}>科幻是未来的钥匙。【科幻题材佳作·收藏必备】</a>
                                 </li> 
                                 <li style={{borderColor: '#3BA94D'}}>
-                                    <a href="https://m.douban.com/doulist/121326/" style={{color: '#3BA94D'}}>美国生活面面观</a>
+                                    <a href="###" style={{color: '#3BA94D'}}>美国生活面面观</a>
                                 </li>
                                 <li style={{borderColor: '#FFAC2D'}}>
-                                    <a href="https://m.douban.com/doulist/37479562/" style={{color: '#FFAC2D'}}>2015终极期待</a>
+                                    <a href="###" style={{color: '#FFAC2D'}}>2015终极期待</a>
                                 </li>
                                 <li style={{borderColor: '#CC3344'}}>
-                                    <a href="https://m.douban.com/doulist/458087/" style={{color: '#CC3344'}}>经典韩国电影——收集100部</a>
+                                    <a href="###" style={{color: '#CC3344'}}>经典韩国电影——收集100部</a>
                                 </li> 
                             </ul>
 
@@ -149,23 +146,23 @@ export default class Movie extends Component {
                         </header>
                         <div className={movie.sectioncontent}>
                             <ul className={movie.typelist}>
-                                <li><a href="/movie/classic">经典<span></span></a></li>
-                                <li><a href="/movie/underrated">冷门佳片<span></span></a></li>
-                                <li><a href="/movie/doubantop">豆瓣高分<span></span></a></li>
-                                <li><a href="/movie/action">动作<span></span></a></li>
-                                <li><a href="/movie/comedy">喜剧<span></span></a></li>
-                                <li><a href="/movie/love">爱情<span></span></a></li>
-                                <li><a href="/movie/mystery">悬疑<span></span></a></li>
-                                <li><a href="/movie/horror">恐怖<span></span></a></li>
-                                <li><a href="/movie/scifi">科幻<span></span></a></li>
-                                <li><a href="/movie/sweet">治愈<span></span></a></li>
-                                <li><a href="/movie/artfilm">文艺<span></span></a></li>
-                                <li><a href="/movie/youth">成长<span></span></a></li>
-                                <li><a href="/movie/animation">动画<span></span></a></li>
-                                <li><a href="/movie/chinese">华语<span></span></a></li>
-                                <li><a href="/movie/western">欧美<span></span></a></li>
-                                <li><a href="/movie/korean">韩国<span></span></a></li>
-                                <li><a href="/movie/japanese">日本<span></span></a></li>
+                                <li><a href="###">经典<span></span></a></li>
+                                <li><a href="###">冷门佳片<span></span></a></li>
+                                <li><a href="###">豆瓣高分<span></span></a></li>
+                                <li><a href="###">动作<span></span></a></li>
+                                <li><a href="###">喜剧<span></span></a></li>
+                                <li><a href="###">爱情<span></span></a></li>
+                                <li><a href="###">悬疑<span></span></a></li>
+                                <li><a href="###">恐怖<span></span></a></li>
+                                <li><a href="###">科幻<span></span></a></li>
+                                <li><a href="###">治愈<span></span></a></li>
+                                <li><a href="###">文艺<span></span></a></li>
+                                <li><a href="###">成长<span></span></a></li>
+                                <li><a href="###">动画<span></span></a></li>
+                                <li><a href="###">华语<span></span></a></li>
+                                <li><a href="###">欧美<span></span></a></li>
+                                <li><a href="###">韩国<span></span></a></li>
+                                <li><a href="###">日本<span></span></a></li>
                             </ul>
                             
                         </div>
--- src/routes/more/More.js
+++ src/routes/more/More.js
@@ -64,9 +64,7 @@ export default class More extends Component {
             }
         
     }
-
     componentDidMount(){
-        // console.log(this.props.match.params.title);
         this.requestData()
         window.addEventListener('scroll', this.handleScroll);
     }
@@ -77,7 +75,6 @@ export default class More extends Component {
     }
 
     handleScroll = (e) => {
-        // console.log(e.srcElement.scrollingElement.scrollHeight,e.srcElement.scrollingElement.scrollTop,e.srcElement.scrollingElement.clientHeight)
         if(e.srcElement.scrollingElement.scrollTop+e.srcElement.scrollingElement.clientHeight+1 >= e.srcElement.scrollingElement.scrollHeight){
             this.setState({page:this.state.page+1});
             this.requestData()
@@ -102,18 +99,16 @@ export default class More extends Component {
                     <Header />
                     <div id={more.moreapp}>
                         <h1>{this.state.list.title}</h1>
-                        <section id={more.list} className={more.grid}>
-                            
+                        <section id={more.list} className={more.grid}> 
                             {
                                 this.state.list.subjects.map((item,index) => {
                                     return (
-                                        <a href="https://m.douban.com/movie/subject/1291561/" className={more.item} key={index}>
+                                        <a href="###" className={more.item} key={index}>
                                             <div className={more.cover}>
                                                 <div className={more.ratio3_4}>
                                                     <img src={item.images.small} alt={item.title} data-x="1080" data-y="1560" className={more.imgshow} style={{"width": "100%"}} />
                                                 </div>
                                             </div>
-                
                                             <div className={more.info}>
                                                 <h3>{item.title}</h3>
                                                 <p className={more.rank}>
@@ -123,13 +118,6 @@ export default class More extends Component {
                                                             return item
                                                         })
                                                     }
-                                                    {/* 
-                                                        <span className={more.ratingstarsmallfull}></span>
-                                                        <span className={more.ratingstarsmallfull}></span>
-                                                        <span className={more.ratingstarsmallfull}></span>
-                                                        <span className={more.ratingstarsmallfull}></span>
-                                                        <span className={more.ratingstarsmallfull}></span>
-                                                     */}
                                                     </span> 
                                                     <span className={more.fen}>{item.rating.average}</span>
                                                 </p>
@@ -138,31 +126,6 @@ export default class More extends Component {
                                     )
                                 })
                             }
-    
-    
-    
-                            {/* <a href="https://m.douban.com/movie/subject/1291561/" className={more.item}>
-                                <div className={more.cover}>
-                                    <div className={more.ratio3_4}>
-                                        <img src="https://img1.doubanio.com/view/photo/m_ratio_poster/public/p2557573348.jpg" alt="千与千寻" data-x="1080" data-y="1560" className={more.imgshow} style={{"width": "100%"}} />
-                                        
-                                    </div>
-                                </div>
-    
-                                <div className={more.info}>
-                                    <h3>千与千寻 </h3>
-                                    <p className={more.rank}>
-                                        <span className={more.ratingstars} data-rating="4.7">
-                                            <span className={more.ratingstarsmallfull}></span>
-                                            <span className={more.ratingstarsmallfull}></span>
-                                            <span className={more.ratingstarsmallfull}></span>
-                                            <span className={more.ratingstarsmallfull}></span>
-                                            <span className={more.ratingstarsmallfull}></span>
-                                        </span> 
-                                        <span className={more.fen}>9.3</span>
-                                    </p>
-                                </div>
-                            </a> */}
                         </section>
                     </div>
                 </div>
